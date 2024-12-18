e762d32a86b12be3a10b04546e89c32510319d20
kymEwhain <ymdwang200@ewhain.net>
2024-12-19 01:02:15 +0900

    Create import.py

---
+++ segmentation/import.py
@@ -0,0 +1,8 @@
+import os
+# if using Apple MPS, fall back to CPU for unsupported ops
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
